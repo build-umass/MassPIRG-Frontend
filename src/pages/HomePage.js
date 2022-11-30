93ec670d36cd95a3cd525bb6a2aa65a616abd2e7
ThomasN12 <phuocthinhdn12023@gmail.com>
2022-11-30 12:10:53 -0500

    add image

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,11 @@
+import Image1 from '../assets/image1.svg';
+
 const HomePage = () => {
-    return <h1>This is the home page</h1>
+    return <>
+        <div className='d-flex justify-content-center my-5'>
+            <img src={Image1} className="img-fluid" alt="Responsive image"></img>
+        </div>
+    </>
 }
 
 export default HomePage;
