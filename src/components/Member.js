fddf3e92d061f6b9584b9f9153f321bd910170ea
ThomasN12 <phuocthinhdn12023@gmail.com>
2022-12-03 14:51:18 -0500

    add images

--- src/components/Member.js
+++ src/components/Member.js
@@ -1,10 +1,11 @@
-import Image from "../assets/ashley.svg";
+// import svgDir from "src/assets";
 import classes from './Member.module.css';
 const Member = (props) => {
     return (
         <div className={`row ${classes.bgGray} my-4`}>
             <div className="col-4">
-                <img src={Image} className="img-fluid" alt="Responsive image"></img>
+                <img src={props.image} className="img-fluid" alt="Responsive image"></img>
+                {/* <img src={Image} className="img-fluid" alt="Responsive image"></img> */}
             </div>
             <div className="col-8">
                 <h3>{props.name}</h3>
