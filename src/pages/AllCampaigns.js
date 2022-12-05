0b20ce4df952f5c1e0d5b50c8b3e989f06a2f529
ThomasN12 <phuocthinhdn12023@gmail.com>
2022-12-05 16:40:35 -0500

    fix card height (temporarily)

--- src/pages/AllCampaigns.js
+++ src/pages/AllCampaigns.js
@@ -4,46 +4,46 @@ import '../components/AllCampaigns.css';
 const AllCampaigns = () => {
     return <>
         <CommonImage />
-        <h1>Current Campaigns</h1>
-        <div class="container">
-            <div class="row gx-5">
-                <div class="col campaign">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title card-text">New Voters Project</h5>
-                            <p class="card-hover-text">
+        <div className="container">
+            <h1 className="d-flex justify-content-center">Current Campaigns</h1>
+            <div className="row gx-5">
+                <div className="col campaign">
+                    <div className="card" style={{height: 200}}>
+                        <div className="card-body">
+                            <h5 className="card-title card-text">New Voters Project</h5>
+                            <p className="card-hover-text">
                                 We Helped Contact Over 50,000 Students To Help Them Vote And Inform Them About What
                                 Is On The Ballot In A Non-Partisan Way
                             </p>
-                            <a href="/#" class="card-hover-text">
+                            <a href="/#" className="card-hover-text">
                                 Click to Learn More
                             </a>
                         </div>
                     </div>
                 </div>
-                <div class="col campaign">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h5 class="card-title card-text">New Voters Project</h5>
-                            <p class="card-hover-text">
+                <div className="col campaign">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title card-text">New Voters Project</h5>
+                            <p className="card-hover-text">
                                 We Helped Contact Over 50,000 Students To Help Them Vote And Inform Them About What
                                 Is On The Ballot In A Non-Partisan Way
                             </p>
-                            <a href="/#" class="card-hover-text">
+                            <a href="/#" className="card-hover-text">
                                 Click to Learn More
                             </a>
                         </div>
                     </div>
                 </div>
-                <div class="col campaign">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title card-text">New Voters Project</h5>
-                            <p class="card-hover-text">
+                <div className="col campaign">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title card-text">New Voters Project</h5>
+                            <p className="card-hover-text">
                                 We Helped Contact Over 50,000 Students To Help Them Vote And Inform Them About What
                                 Is On The Ballot In A Non-Partisan Way
                             </p>
-                            <a href="/#" class="card-hover-text">
+                            <a href="/#" className="card-hover-text">
                                 Click to Learn More
                             </a>
                         </div>
