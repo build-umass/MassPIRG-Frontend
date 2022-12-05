dbb7dcfc340568e612799e6748222b01180dd884
ThomasN12 <phuocthinhdn12023@gmail.com>
2022-12-05 08:58:16 -0500

    fix font

--- src/components/Member.js
+++ src/components/Member.js
@@ -3,14 +3,14 @@ import classes from './Member.module.css';
 const Member = (props) => {
     return (
         <div className={`row ${classes.bgGray} my-4`}>
-            <div className="col-4">
-                <img src={props.image} className="img-fluid" alt="Responsive image"></img>
+            <div className="col-4 my-2">
+                <img src={props.image} className="img-thumbnail" alt="Responsive image"></img>
                 {/* <img src={Image} className="img-fluid" alt="Responsive image"></img> */}
             </div>
-            <div className="col-8">
-                <h3>{props.name}</h3>
+            <div className="col-8 my-3">
+                <h3 className={`${classes.textNameTitle}`}>{props.name}</h3>
                 <p>{props.classAndMajor}</p>
-                <p>{props.email}</p>
+                <p className={`${classes.textEmail}`}>{props.email}</p>
                 <p>{props.description}</p>
             </div>
         </div>
