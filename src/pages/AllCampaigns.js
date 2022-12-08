1089cebb49c7a2254ac03d9e248dbf6226c39035
ThomasN12 <phuocthinhdn12023@gmail.com>
2022-12-08 10:42:38 -0500

    update campaigns content

--- src/pages/AllCampaigns.js
+++ src/pages/AllCampaigns.js
@@ -8,7 +8,7 @@ const AllCampaigns = () => {
             <h1 className="d-flex justify-content-center mb-3">Current Campaigns</h1>
             <div className="row justify-content-center gx-5">
                 <div className="col-3 campaign">
-                    <div className="card" style={{height: 400}}>
+                    <div className="card" style={{height: 600}}>
                         <div className="card-body d-flex align-items-center">
                             <h5 className="card-title card-text display-5">New Voters Project</h5>
                             <span className="card-hover-text">
@@ -24,13 +24,12 @@ const AllCampaigns = () => {
                     </div>
                 </div>
                 <div className="col-3 campaign">
-                    <div className="card" style={{height: 400}}>
+                    <div className="card" style={{height: 600}}>
                         <div className="card-body d-flex align-items-center justify-content-center">
                             <h5 className="card-title card-text display-5">100% Renewable Energy</h5>
                             <span className="card-hover-text">
                             <p>
-                                We Helped Contact Over 50,000 Students To Help Them Vote And Inform Them About What
-                                Is On The Ballot In A Non-Partisan Way
+                                Our generation is the first to feel the impacts of climate change, and only we have the power to mitigate its harmful effects.
                             </p>
                             <a href="/#">
                                 Click to Learn More
@@ -40,13 +39,12 @@ const AllCampaigns = () => {
                     </div>
                 </div>
                 <div className="col-3 campaign">
-                    <div className="card" style={{height: 400}}>
+                    <div className="card" style={{height: 600}}>
                         <div className="card-body d-flex align-items-center justify-content-center">
                             <h5 className="card-title card-text display-5">Make More Textbooks Affordable</h5>
                             <span className="card-hover-text">
                             <p>
-                                We Helped Contact Over 50,000 Students To Help Them Vote And Inform Them About What
-                                Is On The Ballot In A Non-Partisan Way
+                                In order to reduce the financial burden, MASSPIRG advocates for the usage of Open Educational Resources, open source information that professors can utilize to create a free class resource.
                             </p>
                             <a href="/#">
                                 Click to Learn More
